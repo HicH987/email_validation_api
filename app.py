987fb18ce6978c9f53ddc491c42869ad359b8c03
HicH987 <diabhicham987@gmail.com>
2023-11-13 13:49:04 +0100

    use json to make request

--- app.py
+++ app.py
@@ -1,21 +1,20 @@
 from flask import Flask, request, jsonify
 from validate_email import validate_email
 
-
 app = Flask(__name__)
-
 
 @app.route("/")
 def home():
     return "HELLO from flask api"
 
-
-@app.route("/validate_email", methods=["GET"])
+@app.route("/validate_email", methods=["POST"])
 def validate_email_endpoint():
-    email = request.args.get("email")
+    data = request.get_json()  # Get JSON data from the request body
 
-    if email is None:
-        return jsonify({"error": "No email provided in the URL"}), 400
+    if data is None or "email" not in data:
+        return jsonify({"error": "No 'email' provided in the JSON data"}), 400
+
+    email = data["email"]
 
     try:
         is_valid = validate_email(
@@ -28,8 +27,6 @@ def validate_email_endpoint():
         return jsonify({"email": email, "is_valid": is_valid})
     except Exception as e:
         return jsonify({"email": email, "error": str(e)}), 500
-
-
 
 if __name__ == "__main__":
     app.run(debug=False, host="0.0.0.0")
